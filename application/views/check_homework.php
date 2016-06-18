2e981b131e99902ac803da2afbed92cb118dbbcc
Ricter Z <ricterzheng@gmail.com>
2016-06-18 20:56:03 +0800

    beautiful slider bar

--- application/views/check_homework.php
+++ application/views/check_homework.php
@@ -3,18 +3,23 @@
 <div class="homework-detail-card mdl-card mdl-shadow--2dp">
   <div class="mdl-card__title">
     <h2 class="mdl-card__title-text"><?php echo $work->title?> 作业检查情况&nbsp;&nbsp;</h2>
-	<div>
-	<input class="mdl-slider mdl-js-slider" type="range" id="s1" min="10" max="100" value="10" step="10">
-	<br>
-	准确&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;灵敏
-	</div>
 	<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" onclick="check_homework()">
 	  开始检查
-	</button>&nbsp;&nbsp;
+    </button>&nbsp;&nbsp;
     <div class="mdl-spinner mdl-js-spinner is-active check-load image-load-none"></div>
   </div>
   <div class="mdl-card__supporting-text">
-	至少 3 个作业以上才可以生成图片查重结果。
+    至少 3 个作业以上才可以生成图片查重结果。<br><br>
+    灵敏度设置：
+   <div class="process-container">
+    准确
+    <div class="process-bar">
+      <input class="mdl-slider mdl-js-slider" id="range" type="range" min="10" max="100" value="10" tabindex="0" name="range">
+    </div>
+    灵敏
+   </div>
+    <br>
+    <br>
   </div>
   <div class="mdl-card__actions mdl-card--border">
     <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect image-link" onclick="homework_tree()">
@@ -33,13 +38,15 @@
 
 <script>
 function check_homework() {
-        $('.check-load').removeClass('image-load-none');
-	$.get("<?php echo site_url('welcome/ajax_check_homework/' . $work->id);?>",
-	      function(data, status) {
-		  $('#result').text(data);
-		  $('.check-load').hide();
-	      }
-	)
+    $('.check-load').removeClass('image-load-none');
+    var url = "<?php echo site_url('welcome/ajax_check_homework/' . $work->id);?>";
+    url += "?range=" + $("#range").val();
+    $.get(url,
+          function(data, status) {
+              $('#result').text(data);
+              $('.check-load').hide();
+          }
+    )
 }
 
 function homework_tree() {
